c3b3a7ab4d7063f18808fd1d764fe477f5c7245a
alisondesenvolvedor <alisonlima977@gmail.com>
2024-05-15 13:13:00 -0300

    FINAL!

--- script.js
+++ script.js
@@ -11,16 +11,3 @@ document.addEventListener("DOMContentLoaded", function() {
 
   setInterval(slideNext, 3000); // Troca de imagem a cada 3 segundos
 });
-
-document.addEventListener('DOMContentLoaded', () => {
-  const images = document.querySelectorAll('.carousel-image');
-  let currentIndex = 0;
-
-  function showNextImage() {
-      images[currentIndex].classList.remove('active');
-      currentIndex = (currentIndex + 1) % images.length;
-      images[currentIndex].classList.add('active');
-  }
-
-  setInterval(showNextImage, 3000); // Troca de imagem a cada 3 segundos
-});
